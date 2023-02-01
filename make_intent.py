60bbf625f8a5ca8bd8f39f06a4c27ae8c4b5ba08
AleksandrBara <alexbar130379@gmail.com>
2023-02-01 19:44:08 +0300

    Add DialogFlow learning script.

---
+++ make_intent.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+import json
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+
+    with open("questions.json", "r", encoding='utf8') as file:
+        questions_json = file.read()
+
+    questions = json.loads(questions_json)
+
+    for question, phrases in questions.items():
+        if question in 'Устройство на работу':
+            create_intent(
+                project_id,
+                question,
+                phrases['questions'],
+                [phrases['answer']]
+            )
