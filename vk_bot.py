a30fca966b1ea3bc5d64df00bc82150de091275b
AleksandrBara <alexbar130379@gmail.com>
2023-02-02 15:21:48 +0300

    Cut the vkbot's tongue.

--- vk_bot.py
+++ vk_bot.py
@@ -5,23 +5,26 @@ import os
 import random
 from main import detect_intent_texts
 
+
 PROJECT_ID = os.getenv('DIALOGFLOW_PROJECT_ID')
 SESSION_ID = os.getenv('DIALOGFLOW_SESSION_ID')
 
-
 def echo(event, vk_api):
+
     answer = detect_intent_texts(
         PROJECT_ID,
         SESSION_ID,
         event.text,
         language_code='ru-RU'
     )
+    print(answer.intent.is_fallback)
+    if answer.intent.is_fallback:
+        return
     vk_api.messages.send(
         user_id=event.user_id,
-        message=answer,
-        random_id=random.randint(1, 1000)
+        message=answer.fulfillment_text,
+        random_id=random.randint(1,1000)
     )
-
 
 if __name__ == "__main__":
     load_dotenv()
@@ -32,3 +35,4 @@ if __name__ == "__main__":
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             echo(event, vk_api)
+
