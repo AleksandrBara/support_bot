fa1ad054973d30b06ba59ee9e3026e7a1f21073a
AleksandrBara <alexbar130379@gmail.com>
2023-02-03 15:06:12 +0300

    Rename files.

--- get_intent_text.py
+++ get_intent_text.py
@@ -1,17 +1,3 @@
-import logging
-
-
-class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.bot = bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
-
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
     from google.cloud import dialogflow
